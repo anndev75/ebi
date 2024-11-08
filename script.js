15dbade1505a8800501a9e2bd2806f6a906423b4
Angel Guevara <guevara.cjuan23@gmail.com>
2024-11-08 00:22:54 -0500

    Hola Treysy, esto es un commit y quedará grabado en mi github, la web quedará bonita. PARTE2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function() {
     const header = document.getElementById('script-main-header');
     
     // Detectar cuando el usuario ha hecho scroll por debajo del header
-    if (window.scrollY > (window.innerHeight - 75)) {
+    if (window.scrollY > (window.innerHeight - 150)) {
         header.classList.add('sticky-top');
     } else {
         header.classList.remove('sticky-top');
